b79575e188dc7dbed172401d717cba69ec7da1f4
Durval Carvalho <durvalcsouza@outlook.com>
2019-11-27 21:04:27 -0300

    Add Object Orientation

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
 import requests
 
-def get_data(symbol):
 
-    function = "TIME_SERIES_DAILY"
-    apikey = "GWT5VNWYEKMM0NLS"
-    outputsize = "full"
+class APIComunication():
 
-    base_url = "https://www.alphavantage.co/query?function=%s&apikey=%s&symbol=%s&outputsize=%s"
-    base_url = base_url % (function, apikey, symbol, outputsize)
-    response = requests.get(base_url)
+    def __init__(self, apikey):
 
-    data = response.json()
-    data = data["Time Series (Daily)"]
-    data = { date: values["4. close"] for date, values in data.items() }
+        self.base_url = "https://www.alphavantage.co/query?"
+        self.function = "TIME_SERIES_DAILY"
+        self.outputsize = "full"
 
-    return data
+        self.apikey = apikey
+        self.base_url += f"apikey={self.apikey}"
+
+        self.base_url += f"&function={self.function}"
+        self.base_url += f"&outputsize={self.outputsize}"
+        self.base_url += "&symbol=%s"
+
+    def get_stock_prices(self, symbol):
+        response = requests.get(self.base_url % symbol)
+        data = response.json()
+        data = data["Time Series (Daily)"]
+        
+        prices = { date: values["4. close"] for date, values in data.items() }
+        return prices
+
 
 
 # NYSE: New York Stock Exchange
@@ -33,5 +42,7 @@ NYSE = {
     "Under Armour": "UA"
 }
 
-data = get_data(NYSE["Google"])
+APICon = APIComunication(apikey="GWT5VNWYEKMM0NLS")
+prices = APICon.get_stock_prices(NYSE["Google"])
 
+print(prices)
